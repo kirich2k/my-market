1a86c9324f6bfc09635bdee5daca9799dfa259b3
Kirich_2K <Faersan2014@mail.ru>
2023-04-27 19:00:30 +0300

    добавил и применил MaterialUI

--- src/Components/Card/index.tsx
+++ src/Components/Card/index.tsx
@@ -1,3 +1,4 @@
+import {Card as Cards, CardMedia, Typography} from '@material-ui/core'
 import "./Card.css";
 
 type cardProps = {
@@ -8,15 +9,15 @@ type cardProps = {
 
 const Card:React.FC<cardProps> = ({id, img, name}) => {
     return (
-        <div
-            className="card"
+        <Cards
             id={'"' + id + '"'}
+            sx={{boxShadow: 'none', backgroundColor: 'transparent'}}
         >
-            <img src={img} alt={'"' + id + '"'} draggable="false" />
-            <span className="card__description">
+            <CardMedia image={img} component="img" sx={{pointerEvents: "none"}}/>
+            <Typography sx={{borderTop: "1px solid #e5e5e5", transition: "all 0.2s linear", padding: "10px 0px", paddingLeft: '20px', fontFamily: 'default'}}>
                 {name}
-            </span>
-        </div>
+            </Typography>
+        </Cards>
     );
 }
 export default Card;
